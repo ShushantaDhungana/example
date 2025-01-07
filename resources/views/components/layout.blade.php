6b1d638760751f47c3b90c025e0940f6a17a3eb2
Shushanta <shushantadhungana0@gmail.com>
2025-01-07 13:03:11 +0545

    Day-3 Components

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+    <nav>
+        <x-navLink href="/">Home</x-navLink>
+        <x-navLink href="/about" style="color:green;">About</x-navLink>
+        <x-navLink href="/contact">Contact</x-navLink>
+    </nav>
+
+    {{$slot}}
+
+</body>
+</html>
